842aed3ee4362a796e8631c38f98a6653e3f3a2e
user <zhengxin.guo99@gmail.com>
2025-02-07 11:21:59 +0100

    init

---
+++ scripts/services/cloudservices/cloudservice.py
@@ -0,0 +1,4 @@
+from fastapi import FastAPI, Request
+
+
+if __name__ == "__main__":
---
+++ scripts/services/iotservices/services.py
@@ -0,0 +1,6 @@
+## video and microphone sensor sm, 1. data source, 2. handle images and audios
+# iot device : pressure sensor sm, seat sensor sm
+# edge device : detect
+# cloud device :
+
+
