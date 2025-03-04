834a113be4f6e7f8b924215239db99964563ca56
zxg <zhengxin.guo99@gmail.com>
2025-03-04 13:06:51 +0100

    iotservice, edgeservice, cloudservice

--- scripts/services/edgeservices/pressure_pb2.py
+++ scripts/services/edgeservices/pressure_pb2.py
@@ -13,7 +13,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0epressure.proto\x12\x08pressure\"1\n\x05Input\x12\x12\n\nfileNumber\x18\x01 \x01(\x05\x12\x14\n\x0cpressureData\x18\x02 \x03(\x02\";\n\x06Output\x12\x14\n\x0cpressureData\x18\x01 \x03(\x02\x12\x1b\n\x13hasDetectedPressure\x18\x02 \x01(\x08\x62\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0epressure.proto\x12\x08pressure\"1\n\x05Input\x12\x12\n\nfileNumber\x18\x01 \x01(\x05\x12\x14\n\x0cpressureData\x18\x02 \x03(\x02\"N\n\x06Output\x12\x14\n\x0cpressureData\x18\x01 \x03(\x02\x12\x1b\n\x13hasDetectedPressure\x18\x02 \x01(\x08\x12\x11\n\thasThreat\x18\x03 \x01(\x08\x62\x06proto3')
 
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, globals())
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'pressure_pb2', globals())
@@ -23,5 +23,5 @@ if _descriptor._USE_C_DESCRIPTORS == False:
   _INPUT._serialized_start=28
   _INPUT._serialized_end=77
   _OUTPUT._serialized_start=79
-  _OUTPUT._serialized_end=138
+  _OUTPUT._serialized_end=157
 # @@protoc_insertion_point(module_scope)
